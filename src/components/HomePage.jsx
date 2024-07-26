4b248a8376f234dfc43507b143735acf526b6a9a
Luke Hightower <111666694+hightowerbuilds@users.noreply.github.com>
2024-07-25 21:57:52 -0700

    styling on home page

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,17 +1,57 @@
 
 import './HomePage.css'
-import Resume from './Resume';
-
+import { useState } from 'react'
+import Experience from './Experience'
+import PersonalInfo from './PersonalInfo'
 
 export default function HomePage() {
+
+
+  const [ nameToggle, setNameToggle ] = useState(false)
+  const [ employmentToggle, setEmploymentToggle ] = useState(false)
+
+
+
+
+  const handleToggleOpen = () => {
+      setNameToggle(true)
+  }
+
+  const handleToggleClose = () => {
+      setNameToggle(false)
+  }
+
+  const handleEmpToggleOpen = () => {
+      setEmploymentToggle(true)
+  }
+
+  const handleEmpToggleClose = () => {
+      setEmploymentToggle(false)
+  }
+
+
+
+
+
+
   return (
     <div className='mainBox'>
 
 
       <div className='sideBar'>
-     
+        { nameToggle ? <button className='openInfoButton' onClick={handleToggleClose}>close info</button> :   <button className='openInfoButton' onClick={handleToggleOpen}>personal info</button>}
+        { employmentToggle ? <button className='openEmpButton' onClick={handleEmpToggleClose}>close info</button> : <button className='openEmpButton' onClick={handleEmpToggleOpen}>employment experience</button>}
       </div>
        
+      <div className='mainHomeSection'>
+      { nameToggle ?  <PersonalInfo />  : ''}
+      { employmentToggle ? <Experience /> : ''}
+      </div>
+
     </div>
   )
 }
+
+
+
+
