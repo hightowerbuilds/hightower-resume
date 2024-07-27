182cc0e06bbe2d479599db86bb054daa91eac622
Luke Hightower <111666694+hightowerbuilds@users.noreply.github.com>
2024-07-26 20:49:42 -0700

    setting up layout for creatving wrting links yay

--- src/components/CreativeWriting.jsx
+++ src/components/CreativeWriting.jsx
@@ -4,9 +4,17 @@ import './CreativeWriting.css'
 export default function CreativeWriting() {
   return (
     <div>
-        <h1>Creative Writing</h1>
-
+        <h1 className='creativeWritingTitle'>Creative Writing</h1>
+        <p className='creativeWritingLinkList'>
         <Link to='/PrettyNoPerspective'>pretty book</Link>
+        <br />  
+        <Link to='/PrettyNoPerspective'>pretty book</Link>
+        <br />
+        <Link to='/PrettyNoPerspective'>pretty book</Link>
+        <br />
+        <Link to='/PrettyNoPerspective'>pretty book</Link>
+        </p>
+       
 
 
     </div>
