9c80de59560de8769f81a2b144895aaf4660bfc5
Luke Hightower <111666694+hightowerbuilds@users.noreply.github.com>
2024-10-24 20:44:01 -0700

    adding brontosaurus link to web dev page

--- src/components/Development/Development.jsx
+++ src/components/Development/Development.jsx
@@ -6,10 +6,13 @@ export default function Development() {
       <h2 className='webDevTitle'>projects in web development</h2>
       <ul style={{ marginLeft: '5%'}}>
         <li>
-          <a href="https://the-slurping-slug-app.vercel.app/">SlugLicker (band & graphic art)</a>
+          <a href="https://the-slurping-slug-app.vercel.app/" target='blank'>Sluglicker (band & graphic art)</a>
         </li>
         <li>
-          <a href="https://smash-burger-app.vercel.app/">Smash Burger Bros. (food truck)</a>
+          <a href="https://smash-burger-app.vercel.app/" target='blank'>Smash Burger Bros. (food truck)</a>
+        </li>
+        <li>
+          <a href="https://brontosaurus-publications.vercel.app/" target='blank'>Brontosaurus Publications</a>
         </li>
       </ul>
     </div>
