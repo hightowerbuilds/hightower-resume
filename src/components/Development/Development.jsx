261d71603c7907f259913c6d1f5ed95ca98ac907
Luke Hightower <111666694+hightowerbuilds@users.noreply.github.com>
2024-10-24 21:00:23 -0700

    adding image to slug link

--- src/components/Development/Development.jsx
+++ src/components/Development/Development.jsx
@@ -1,13 +1,16 @@
 import './Development.css'
+import sluglicker from '../../images/sluglicker.jpg'
 
 export default function Development() {
   return (
     <div>
       <h2 className='webDevTitle'>projects in web development</h2>
       <ul style={{ marginLeft: '5%'}}>
-        <li>
-          <a href="https://the-slurping-slug-app.vercel.app/" target='blank'>Sluglicker (band & graphic art)</a>
-        </li>
+        <p>
+          <a href="https://the-slurping-slug-app.vercel.app/" target='blank'><img style={{borderRadius: '2%'}} src={sluglicker}/></a>
+          <br />
+          <p>(band, graphic artist)</p>
+        </p>
         <li>
           <a href="https://smash-burger-app.vercel.app/" target='blank'>Smash Burger Bros. (food truck)</a>
         </li>
